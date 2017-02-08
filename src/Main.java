32259f0f9cff41b03e1e22358e8a97210d4a06eb
Nicole D Payne <npain@netscape.com>
2017-02-08 00:28:06 -0500

    Pending

---
+++ src/Main.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nicole on 2/3/2017.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Pig Latin Translator!");
+
+        String answer;
+
+        //loop to keep translating if the user enters "yes"
+        do {
+            System.out.println("Enter a word to translate: ");
+            String word = scan.next();
+
+            //translate the word
+            String translated = translate(word);
+
+            //output it
+            System.out.println(translated);
+
+            System.out.print("Do you want to continue? yes/no ");
+
+            //get the response, turn it lower case
+            answer = scan.next().toLowerCase();
+
+        } while (answer.equals("yes"));
+
+        scan.close();
+    }
+
+    public static String translate(String word) {
+        //find the first vowel
+        int vowel = findVowel(word);
+        //if it's an invalid word
+        if (vowel == -1) {
+            return "invalid word";
+
+            //if it's at pos. 0
+            //add "way" to the end
+        } else if (vowel == 0) {
+            return word + "way";
+            //else
+            //take the substring from the vowel to the end
+            //add the substring from 0 until vowel pos. - 1
+            //add "ay"
+
+        } else {
+            return word.substring(vowel, word.length()) + word.substring(0, vowel) + "ay";
+        }
+    }
+
+    public static int findVowel(String word) {
+
+        //for loop i from position 0 to 4 (or end of word)
+        //for loops create a sequential counter for you
+        //use charAt() to get the letter
+        //if it's a 'or' 'e' or 'i' or 'o' or 'u'
+        //return position(variable i)
+
+        for  (int i = 0; i < word.length(); i++) {
+          if ((word.charAt(i) == 'a') || (word.charAt(i) == 'e' || (word.charAt(i) == 'i'
+                  || (word.charAt(i) == 'o' || (word.charAt(i) == 'u')) {
+                return i;
+          }
+
+            //if not vowel found
+            return -1;
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Practice.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nicole on 2/7/2017.
+ */
+public class Practice {
+    public static void main(String[] args) {
+        //output a count from 1 to 1000
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+        }
+
+        //output a count from 200 to 0 by fours (200 196 192... 0)
+        for (int i = 200; i >= 0; i -= 4) {
+            System.out.println(i);
+        }
+
+        //take an int from user
+        //keep prompting then for an int until it's between 1 AND 10
+        Scanner scan = new Scanner(System.in);
+        {
+            int number;
+            do {
+                System.out.print("Please enter an integer between 1 and 10");
+                number = scan.nextInt();
+            } while (number < 1 || number > 10);
+
+            System.out.println("Thank you; your number is " + number);
+
+            String sentence = "Hello world";
+            //output the contents of sentence until the first space
+            //find a space
+            int firstSpace;
+            firstSpace = sentence.indexOf('');
+            //get the contents up until the first space
+            // note: this will not include the space in substring
+            String firstWord = sentence.substring(0, firstSpace);
+            //output
+            System.out.println(firstWord);
+
+            //System.out.println();
+            String w = firstWord ("this weeks with any sentence");
+            System.out.println(w);
+
+            //write a method that takes a String argument and returns the first word
+            //call the method firstWord
+            public static String firstWord(Strings) {
+
+            //find a space
+            int firstSpace = s.indexOf('');
+
+            //get the contents up until the first space
+            // note: this will not include the space in substring
+            String firstSpace = s.substring(0, firstSpace);
+
+            //output
+            return firstWord;
+        }
+
+
+        }
+
+
+
+    }
+}
